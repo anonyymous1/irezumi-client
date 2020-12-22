7b9523734937641b4e38076c8fbacb34b26b906c
swolepenguin <ashtonm223@gmail.com>
2020-12-22 09:57:13 -0500

    welcome page changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,8 @@ import React from "react";
 const Welcome = () => {
     return (
         <div>
-            <h1>Welcome</h1>
+            <h1>Welcome To Irezumi</h1>
+            <h3>At Irezumi you can find the best parlors and artists around as well as gain some inspiration for what exactly it is that you want and where.</h3>
         </div>
     )
 }
