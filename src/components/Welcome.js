29065643c33e8dd38db54545df7a4a6d0be6104e
Amari James <mar.james1998@gmail.com>
2020-12-22 12:16:09 -0500

    trying-again

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,50 @@
 import React from "react";
+
+
 const Welcome = () => {
     return (
         <div>
             <h1>Welcome To Irezumi</h1>
-            <h3>At Irezumi you can find the best parlors and artists around as well as gain some inspiration for what exactly it is that you want and where.</h3>
+            <h3>At Irezumi you can find the best parlors and artists around as well as gain some inspiration for tattoos that you want and the best places to go to get them done.</h3>
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="https://res-3.cloudinary.com/dostuff-media/image/upload//w_1200,q_75,c_limit,f_auto/v1538078074/page-image-4703-b932c90f-cd31-4155-9f56-70fbb71dbf66.jpg" class="d-block w-100" alt="..."></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.badhabitstattoos.com/wp-content/uploads/2019/04/lase-tattoo-removal-1024x640.png" class="d-block w-100" alt="..."></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://i.imgur.com/kmyUv3v.jpg" class="d-block w-100" alt="..."></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://i.imgur.com/McfDdAO.jpg" class="d-block w-100" alt="..."></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://i.imgur.com/GlMuTc9.jpg" class="d-block w-100" alt="..."></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://i.pinimg.com/originals/12/e7/22/12e7225de596357a8247524ab2b29d2b.jpg" class="d-block w-100" alt="..."></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://images.unsplash.com/photo-1561377455-190afb395ed7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" class="d-block w-100" alt="..."></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://mymodernmet.com/wp/wp-content/uploads/2019/09/esther-garcia-tattoos-2.jpg" class="d-block w-100" alt="..."></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.wildtattooart.com/wp-content/uploads/2017/03/wolf-tattoos-12031771.jpg" class="d-block w-100" alt="..."></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://mymodernmet.com/wp/wp-content/uploads/2017/06/traditional-japanese-tattoo-1.jpg" class="d-block w-100" alt="..."></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.inkme.tattoo/wp-content/uploads/2015/11/Japanese-tattoos-09031744.jpg" class="d-block w-100" alt="..."></img>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
 export default Welcome;
+
