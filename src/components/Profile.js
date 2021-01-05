34de6347f203ff85e8b8f8583e7c816c5e7a36cf
alexjbustillos <alexjbustillos@gmail.com>
2021-01-05 11:22:00 -0500

    [profile]add imageURL to profile and cloudinary to profileedit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,13 +2,14 @@ import { Link } from 'react-router-dom'
 
 const Profile = (props) => {
 
-    // console.log(props);
+    console.log(props.user.imageURL);
     const userData = props.user ?
     (<div>
         <h1>Profile</h1>
         <p><strong>Name:</strong> { props.user.name } </p>
         <p><strong>Email:</strong> { props.user.email } <Link to="/profileedit">edit</Link></p>
         <p><strong>ID:</strong> { props.user.id } </p>
+        <img src={props.user.imageURL} alt={props.user.name}/>
     </div>) : <h4>Loading...</h4>
 
     const errorDiv = () => {
